99e0c1874b8a254d9780e98d0e94696050ec1c2a
karimoffabdulatif <akarimoff001@gmail.com>
2025-06-21 15:48:09 +0500

    ch

--- app/(root)/components/header.jsx
+++ app/(root)/components/header.jsx
@@ -41,8 +41,8 @@ export default function Header() {
 	const selectedLangObj = languages.find(lang => lang.label === selectedLang)
 
 	return (
-		<header className="fixed mt-2 sm:mt-0 left-0 w-full z-50 bg-transparent text-white">
-			<div className="container mx-auto px-14 mt-[-50px] flex items-center justify-between py-4 relative">
+		<header className="fixed mt-2 sm:mt-0 w-full z-50 bg-transparent text-white">
+			<div className="container mx-auto px-2 sm:px-14 mt-[-50px] flex items-center justify-between py-4 relative">
 
 				{/* Logo */}
 				<div className="flex ml-3  sm:ml-0 w-[140px] items-center mt-4 sm:mt-0 sm:w-[35%]">
